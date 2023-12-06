99b6aa8afb6d6f1a397bb97fa40ff2cf88384ae5
Aaron Gesmer <aaron.gesmer@gmail.com>
2023-12-06 22:24:32 +0000

    chore: adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
